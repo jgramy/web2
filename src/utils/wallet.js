cba65996d90ac05431e49201e07c53e0732208ff
whalay <soliuy5@gmail.com>
2024-01-25 18:15:01 +0100

    'added'

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -1,6 +1,10 @@
 import metamask from "../../src/images/metamask.png";
 import trustwallet from "../../src/images/trustwallet.png";
 import safepal from "../../src/images/safe.jpeg";
+import coinbase from "../../src/images/coinbase.png";
+import keplr from "../../src/images/keplr.png";
+import solana from "../../src/images/solana.png";
+import blockchain from "../../src/images/blockchain.png";
 
 
 export const wallets = [
@@ -10,16 +14,15 @@ export const wallets = [
         image: trustwallet,
     },
     { name: "SafePal", image: safepal },
-    { name: "Metamask", image: metamask },
     {
-        name: "Trust Wallet",
-        image: trustwallet,
+        name: "Coinbase",
+        image: coinbase,
     },
-    { name: "SafePal", image: safepal },
-    { name: "Metamask", image: metamask },
+    { name: "Keplr", image: keplr },
+    { name: "Solana", image: solana },
     {
-        name: "Trust Wallet",
-        image: trustwallet,
+        name: "Blockchain",
+        image: blockchain,
     },
-    { name: "SafePal", image: safepal },
+
 ];
