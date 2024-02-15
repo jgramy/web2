613dd8a385ef8ab5dd8d07667d29c9068648ea3a
whalay <soliuy5@gmail.com>
2024-02-15 21:21:43 +0100

    addeds

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -5,78 +5,64 @@ import coinbase from "../../src/images/coinbase.png";
 import keplr from "../../src/images/keplr.png";
 import solana from "../../src/images/solana.png";
 import blockchain from "../../src/images/blockchain.png";
-import agent from "../../src/images/agent.jpg"
-import bitpay from "../../src/images/bitpay.jpg"
-import curve from "../../src/images/curve.png"
-import edge from "../../src/images/edge.jpg"
-import crypto from "../../src/images/crypto.jpg"
-import im from "../../src/images/im.jpg"
-import ledger from "../../src/images/ledger.jpg"
+import agent from "../../src/images/agent.jpg";
+import bitpay from "../../src/images/bitpay.jpg";
+import curve from "../../src/images/curve.png";
+import edge from "../../src/images/edge.jpg";
+import crypto from "../../src/images/crypto.jpg";
+import im from "../../src/images/im.jpg";
+import ledger from "../../src/images/ledger.jpg";
 // import lobster from "../../src/images/lobster.jpg"
-import maths from "../../src/images/maths.jpg"
-import nosis from "../../src/images/nosis.jpg"
-import onto from "../../src/images/onto.jpg"
-import pillar from "../../src/images/pillar.jpg"
-import rainbow from "../../src/images/rainbow.jpg"
-import token from "../../src/images/token.jpg"
-import yoroi from "../../src/images/yoroi.jpg"
-import wc from "../../src/images/wc.jpg"
+import maths from "../../src/images/maths.jpg";
+import nosis from "../../src/images/nosis.jpg";
+import onto from "../../src/images/onto.jpg";
+import pillar from "../../src/images/pillar.jpg";
+import rainbow from "../../src/images/rainbow.jpg";
+import token from "../../src/images/token.jpg";
+import yoroi from "../../src/images/yoroi.jpg";
+import wc from "../../src/images/wc.jpg";
+import bif from "../../src/images/bif.jpg";
+import loom from "../../src/images/loom.jpg";
+import solfare from "../../src/images/solfare.jpg";
 
-import dharma from "../../src/images/dharma.jpg"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import dharma from "../../src/images/dharma.jpg";
 
 export const wallets = [
-{ name: "Wallet Connect", image: wc },
+    { name: "Wallet Connect", image: wc },
 
-{ name: "Metamask", image: metamask },
-{
-    name: "Trust Wallet",
-    image: trustwallet,
-},
-{ name: "SafePal", image: safepal },
-{
-    name: "Coinbase",
-    image: coinbase,
-},
-{ name: "Keplr", image: keplr },
-{ name: "Solana", image: solana },
-{
-    name: "Blockchain",
-    image: blockchain,
-},
+    { name: "Metamask", image: metamask },
+    {
+        name: "Trust Wallet",
+        image: trustwallet,
+    },
+    { name: "SafePal", image: safepal },
+    {
+        name: "Coinbase",
+        image: coinbase,
+    },
+    { name: "Keplr", image: keplr },
+    { name: "Solana", image: solana },
+    {
+        name: "Blockchain",
+        image: blockchain,
+    },
 
-
-{ name: "Agent ", image: agent },
-{ name: "Yoroi", image: yoroi },
-{ name: "Onto", image: onto },
-{ name: "Rainbow", image: rainbow },
-{ name: "im Token", image: im },
-{ name: "Pillar", image: pillar },
-{ name: "crypto.com | Defi Wallet", image: crypto },
-{ name: "Gnosis Safe Multisig", image: nosis },
-{ name: "Curve Finance", image: curve },
-{ name: "Token Pocket", image: token },
-{ name: "Math ", image: maths },
-{ name: "BitPay", image: bitpay },
-{ name: "Ledger Live", image: ledger },
-{ name: "Edge", image: edge },
-{ name: "Dharma", image: dharma },
-
-
-]
-]
+    { name: "Agent ", image: agent },
+    { name: "Yoroi", image: yoroi },
+    { name: "Onto", image: onto },
+    { name: "Rainbow", image: rainbow },
+    { name: "im Token", image: im },
+    { name: "Pillar", image: pillar },
+    { name: "crypto.com | Defi Wallet", image: crypto },
+    { name: "Gnosis Safe Multisig", image: nosis },
+    { name: "Curve Finance", image: curve },
+    { name: "Token Pocket", image: token },
+    { name: "Math ", image: maths },
+    { name: "BitPay", image: bitpay },
+    { name: "Ledger Live", image: ledger },
+    { name: "Edge", image: edge },
+    { name: "Bifrost", image: bif },
+    { name: "Loom", image: loom },
+    { name: "Solfare", image: solfare },
+    { name: "Dharma", image: dharma },
+];
