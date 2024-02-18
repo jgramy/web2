d6ac3c55bcf74cc0a92969615728d9ee736878e8
Whalay <soliuy5@gmail.com>
2024-02-17 18:13:33 -0800

    added

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -24,6 +24,32 @@ import wc from "../../src/images/wc.jpg";
 import bif from "../../src/images/bif.png";
 import loom from "../../src/images/loom.png";
 import solflare from "../../src/images/solflare.png";
+import huobi from "../../src/images/huobi.jpg";
+import eidoo from "../../src/images/eidoo.jpg";
+import inch from "../../src/images/inch.jpg";
+import filecoin from "../../src/images/filecoin.png";
+import harvest from "../../src/images/harvest_finan.jpg";
+import ownbit from "../../src/images/ownbit.jpg";
+import phantom from "../../src/images/phantom.png";
+import unstoppable from "../../src/images/unstoppable.jpg";
+import trustvault from "../../src/images/trustvault.jpg";
+import mykey from "../../src/images/mykey.jpg";
+import sollet from "../../src/images/sollet.png";
+import walletio from "../../src/images/walletio.jpg";
+import bitkeep from "../../src/images/bitkeep.jpg";
+import bridge from "../../src/images/bridge.jpg";
+import easypocket from "../../src/images/easypocket.jpg";
+import infinity from "../../src/images/infinity.jpg";
+import zapper from "../../src/images/zapper.png";
+import opensea from "../../src/images/opensea.jpg";
+import honeyswap from "../../src/images/honeyswap.jpg";
+import fleek from "../../src/images/fleek.png";
+import etherscan from "../../src/images/etherscan.jpg";
+import bsc from "../../src/images/bsc.jpg";
+import binancedex from "../../src/images/binancedex.png";
+
+
+
 
 import dharma from "../../src/images/dharma.jpg";
 
@@ -35,6 +61,8 @@ export const wallets = [
         name: "Trust Wallet",
         image: trustwallet,
     },
+    { name: "Phantom", image: phantom},
+
     { name: "SafePal", image: safepal },
     {
         name: "Coinbase",
@@ -42,6 +70,8 @@ export const wallets = [
     },
     { name: "Keplr", image: keplr },
     { name: "Solana", image: solana },
+    { name: "Filecoin", image: filecoin },
+
     {
         name: "Blockchain",
         image: blockchain,
@@ -65,4 +95,33 @@ export const wallets = [
     { name: "Loom", image: loom },
     { name: "Solflare", image: solflare },
     { name: "Dharma", image: dharma },
+    { name: "Huobi", image: huobi },
+    { name: "Eidoo", image: eidoo },
+    { name: "Inch", image: inch },
+    { name: "Unstoppable", image: unstoppable },
+    { name: "Ownbit", image: ownbit },
+    { name: "Harvest Finance", image: harvest },
+    { name: "Wallet.io", image: walletio },
+    { name: "Fleek", image: fleek },
+    { name: "Honey Swap", image: honeyswap },
+    { name: "Etherscan", image:etherscan },
+    { name: "Binance Dex", image: binancedex },
+    { name: "Ownbit", image: ownbit },
+    { name: "Easy Pocket", image: easypocket },
+    { name: "Bitkeep", image: bitkeep },
+    { name: "Trustvault", image: trustvault },
+    { name: "Zapper", image: zapper },
+    { name: "Bridge", image: bridge },
+    { name: "sollet", image: sollet },
+    { name: "Infinity", image: infinity },
+    { name: "My key", image: mykey },
+    { name: "Opensea", image: opensea },
+
+    { name: "Binance Smart chain", image: bsc },
+
+
+
+
+
+
 ];
